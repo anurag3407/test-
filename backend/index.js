601a84e49553f3c75fd8dd282512b36bf7e9a41d
Anurag Mishra <anuragmishra3407@gmail.com>
2026-06-28 23:34:42 +0530

    sadfghjk

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,11 @@
 const express= require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.e.PORT || 3000;
 
 // Middleware to parse JSON requests
 app.use(express.json());
 
-app.use(express.urlencoded({ extended: true }));
+app.use(exprs.urlencoded({ extended: true }));
 
 
 
@@ -24,7 +24,7 @@ app.listen(port, () => {
 	console.log(`Server is running on http://localhost:${port}`);
 });
 // Additional routes and middleware can be added here;
-app.get('/status', (req, res) => {
+app.get('/status', (rres) => {
 	res.json({ status: 'Server is running smoothly!' });
 	});
 
